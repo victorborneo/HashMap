4ca32d9898d452b9103b2d32905a043a5b7823a2
Victor Vieira <victorbor2001@yahoo.com.br>
2023-08-03 23:32:16 -0300

    Changed underscore conventions

--- HashMap.py
+++ HashMap.py
@@ -10,15 +10,15 @@ Value = Any
 
 class HashMap:
     def __init__(self, func: Callable=None, size: int=2**16):
-        self.__len = 0
-        self.__size = size
-        self.__hash_func = hash if func is None else func
-        self.__map = [[] for _ in range(size)]
+        self._len = 0
+        self._size = size
+        self._hash_func = hash if func is None else func
+        self._map = [[] for _ in range(size)]
 
     def __str__(self):
         string = []
 
-        for arr in self.__map:
+        for arr in self._map:
             for key, val in arr:
                 string.append(f"{key}: {val}")
 
@@ -28,51 +28,51 @@ class HashMap:
         return "\n".join(string)
 
     def __len__(self):
-        return self.__len
+        return self._len
 
-    def __get_idx__(self, map_key: Any) -> None:
-        return self.__hash_func(map_key) % self.__size
+    def _get_idx(self, map_key: Any) -> None:
+        return self._hash_func(map_key) % self._size
 
     def get(self, map_key: Any, not_found_value=None) -> Value:
-        idx = self.__get_idx__(map_key)
-        for key, value in self.__map[idx]:
+        idx = self._get_idx(map_key)
+        for key, value in self._map[idx]:
             if map_key == key:
                 return value
         return not_found_value
 
     def remove(self, map_key: Any) -> None:
-        idx = self.__get_idx__(map_key)
-        for c, (key, _) in enumerate(self.__map[idx]):
+        idx = self._get_idx(map_key)
+        for c, (key, _) in enumerate(self._map[idx]):
             if map_key == key:
-                self.__len -= 1
-                self.__map[idx].pop(c)
+                self._len -= 1
+                self._map[idx].pop(c)
                 return
         raise KeyError("Key not found in HashMap")
 
     def add(self, map_key: Any, map_value: Any) -> None:
-        idx = self.__get_idx__(map_key)
-        for c, (key, _) in enumerate(self.__map[idx]):
+        idx = self._get_idx(map_key)
+        for c, (key, _) in enumerate(self._map[idx]):
             if map_key == key:
-                self.__map[idx][c] = (map_key, map_value)
+                self._map[idx][c] = (map_key, map_value)
                 return
 
-        self.__len += 1
-        self.__map[idx].append((map_key, map_value))
-        if self.__len >= self.__size:
-            self.__map.extend([[] for _ in range(self.__size)])
-            self.__size *= 2
+        self._len += 1
+        self._map[idx].append((map_key, map_value))
+        if self._len >= self._size:
+            self._map.extend([[] for _ in range(self._size)])
+            self._size *= 2
 
     def keys(self) -> Generator[Key, None, None]:
-        for arr in self.__map:
+        for arr in self._map:
             for tup in arr:
                 yield tup[0]
 
     def values(self) -> Generator[Value, None, None]:
-        for arr in self.__map:
+        for arr in self._map:
             for tup in arr:
                 yield tup[1]
 
     def items(self) -> Generator[Tuple[Key, Value], None, None]:
-        for arr in self.__map:
+        for arr in self._map:
             for tup in arr:
                 yield tup
